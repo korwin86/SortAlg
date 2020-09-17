0f80c0d2092fed64ce3e0f2008833840fcc0fecf
Александр Семенов <korwin86@gmail.com>
2020-09-17 14:56:04 +0700

    Сортировка пузырьком

---
+++ AlgorithmTests/BubbleSortTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class BubbleSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            //arrange
+
+            var bubble = new BubbleSort<int>();
+
+            var rnd = new Random();
+            var items = new List<int>();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                items.Add(rnd.Next(0, 1000));
+            }
+
+            bubble.Items.AddRange(items);
+            items.Sort();
+
+            //act
+
+            bubble.Sort();
+
+            //assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(items[i], bubble.Items[i]);
+            }
+            
+        }
+    }
+}
--- SortAlg/Program.cs
+++ SortAlg/Program.cs
@@ -18,5 +18,6 @@ namespace SortAlg
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+        
     }
 }
